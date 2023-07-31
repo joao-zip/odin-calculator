1d36b8e0bea23f94c434293efa94efe23db2fb63
joao-zip <oliveira.joao@unifesp.br>
2023-07-31 16:22:45 -0300

    refactor: changed expression loading

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ let sum = (number1, number2) => number1 + number2,
     subtract = (number1, number2) => number1 - number2,
     multiply = (number1, number2) => number1 * number2,
     divide = (number1, number2) => number1 / number2,
-    percent = (percentage, total) => percentage * 0.01 * total;
+    percent = (percentage, total) => percentage * 0.01 * total,
+    operate = (operator, number1, number2) => {
+        switch (operator) {
+            case '+':
+                return sum(number1, number2);
+            case '-':
+                return subtract(number1, number2);
+            case 'x':
+                return multiply(number1, number2);
+            case '\\':
+                return divide(number1, number2);
+            case '%':
+                return percent(number1, number2);
+            default:
+                return null;
+        }
+    };
 
 let doOperation = document.querySelector('.equals'),
     digits = document.querySelectorAll('.digit'),
@@ -12,14 +28,18 @@ let doOperation = document.querySelector('.equals'),
 
 let display = document.querySelector('.display'),
     expression = document.querySelector('.expression'),
-    result = document.querySelector('.result');
+    result = document.querySelector('.result'),
+    number1, number2;
 
 allClear.addEventListener('click', () => {
     expression.innerHTML = '';
 });
 
 clear.addEventListener('click', () => {
-    expression.innerHTML = expression.innerHTML.slice(0, -1);
+    if(expression.innerHTML.at(-1) == ' ')   
+        expression.innerHTML = expression.innerHTML.slice(0, -3);
+    else
+        expression.innerHTML = expression.innerHTML.slice(0, -1);
 });
 
 digits.forEach(element => {
@@ -30,6 +50,6 @@ digits.forEach(element => {
 
 operators.forEach(element => {
     element.addEventListener('click', () => {
-        expression.innerHTML += element.innerHTML;
+        expression.innerHTML += (' ' + element.innerHTML + ' ');
     });
 });
